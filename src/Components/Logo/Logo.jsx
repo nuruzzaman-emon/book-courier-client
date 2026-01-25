dd955d3fb6dd1f9bfffa6dedd2fcd4524e7909c0
unknown <emon21108@gmail.com>
2026-01-25 23:14:57 +0600

    polishing navbar

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router";
 const Logo = () => {
   return (
     <Link to="/">
-      <div className="btn btn-xs md:btn-md btn-primary px-3  py-3">
+      <div className="btn btn-xs md:btn-md btn-primary px-3  py-4">
         {/* Animated book icon */}
         <BiSolidBookOpen className="text-white animate-pulse w-4 h-4 md:w-10 md:h-10" />
         <h2 className="font-extrabold text-xs md:text-xl text-white tracking-wide drop-shadow-lg animate-pulse">
